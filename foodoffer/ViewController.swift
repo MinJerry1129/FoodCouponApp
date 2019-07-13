c5ebf64b8d1d2706055ce6260a84342edfe665be
Admin <admin@Mac-Admin.local>
2019-07-13 16:15:59 +0300

    Initial Commit

---
+++ foodoffer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodoffer
+//
+//  Created by Admin on 7/13/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
